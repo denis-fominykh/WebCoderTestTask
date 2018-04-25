2becfe52bec2b2d5037f60df4adb4d6f864876c7
Denis <fominykhdenis@gmail.com>
2018-04-25 22:48:44 +0300

    Start project

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+//Less to css
+gulp.task('less', function () {
+	gulp.src('./src/styles/**/style.less')
+			.pipe(less())
+			.pipe(gulp.dest('./dist/css/'));
+});
+
+//Sass to css
+gulp.task('sass', function () {
+	gulp.src('./src/styles/**/style.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./dist/css/'));
+});
+
+//Less files change
+gulp.task('less:watch', function () {
+	gulp.watch('./src/styles/**/style.less', ['less']);
+});
+
+//Sass files change
+gulp.task('sass:watch', function () {
+	gulp.watch('./src/styles/**/style.scss', ['sass']);
+});
